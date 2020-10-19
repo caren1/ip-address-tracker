e9436a24dda60d99773d47ef62753bc81b868143
caren1 <czarnocki.w@wp.pl>
2020-10-19 14:43:11 +0200

    initalized leaflet package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import './App.css';
 
 function App() {
+
+
+  const position = [51.505, -0.09];
+  const zoom = 13;
+
   return (
     <div className="app">
       <header className="tracker">
@@ -32,9 +38,15 @@ function App() {
             <strong><p>SpaceX Starlink</p></strong>
           </div>
         </div>
-        <div class="map">
-          <h1>will be map here</h1>
-        </div>
+        <Map center={position} zoom={13}>
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+    <Marker position={position}>
+      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+    </Marker>
+  </Map>
       </main>
     </div>
   );
