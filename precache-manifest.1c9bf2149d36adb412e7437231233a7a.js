fb03783584c10b5adc5c4fb5eb59948df0c35bfa
caren1 <czarnocki.w@wp.pl>
2020-10-20 09:12:39 +0200

    Updates

---
+++ precache-manifest.1c9bf2149d36adb412e7437231233a7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d712af6e597b357e9015321bf28c9b",
+    "url": "/ip-address-tracker/index.html"
+  },
+  {
+    "revision": "e659a4078da9b9ae0106",
+    "url": "/ip-address-tracker/static/css/main.a3940bf1.chunk.css"
+  },
+  {
+    "revision": "1f71e5e80dd27cb2109d",
+    "url": "/ip-address-tracker/static/js/2.e910e1d7.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/ip-address-tracker/static/js/2.e910e1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e659a4078da9b9ae0106",
+    "url": "/ip-address-tracker/static/js/main.75662504.chunk.js"
+  },
+  {
+    "revision": "5edf9300e4981cca50c1",
+    "url": "/ip-address-tracker/static/js/runtime-main.b78ae688.js"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/ip-address-tracker/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
